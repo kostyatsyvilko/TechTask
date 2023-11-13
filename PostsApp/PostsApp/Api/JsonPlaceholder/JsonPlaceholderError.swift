11ace8217051b49d7ac308560e7af3fca508e56b
kostya <kostya.tsyvilko@innowise-group.com>
2023-11-13 09:10:22 +0100

    refactor: add error descriptions

--- PostsApp/PostsApp/Api/JsonPlaceholder/JsonPlaceholderError.swift
+++ PostsApp/PostsApp/Api/JsonPlaceholder/JsonPlaceholderError.swift
@@ -1,8 +1,21 @@
 import Foundation
 
-enum JsonPlaceholderError: Error {
+enum JsonPlaceholderError: Error, LocalizedError {
     case encoding
     case decoding
     case incorrectUrl
     case server(message: Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .encoding:
+            return "Cannot encode value"
+        case .decoding:
+            return "Cannot decode value"
+        case .incorrectUrl:
+            return "Incorrect url"
+        case .server(let message):
+            return message.localizedDescription
+        }
+    }
 }
